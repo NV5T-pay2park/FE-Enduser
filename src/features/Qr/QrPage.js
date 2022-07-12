6352a56c08ca2650cf70056ac9f8d666badc5b4b
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-07-12 11:27:51 +0700

    add QR screen

--- src/features/Qr/QrPage.js
+++ src/features/Qr/QrPage.js
@@ -1,15 +1,25 @@
 import React from 'react'
-
+import { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
 const QrPage = () => {
-  return (
-    <div>
-        <div>QrPage</div>
-        <div>QrPage</div>
-        <div>QrPage</div>
-        <div>QrPage</div>
-        <div>QrPage</div>
 
-    </div>
+  const [scanResult, setScanResult] = useState('');
+
+  const handleErrorCam = (error) => {
+    console.log(error);
+  }
+
+  const handleScanCam = (result) => {
+    if (result) {
+      setScanResult(result);
+    }
+  }
+
+  return (
+    <div className="App">
+    <QrReader delay={300} style={{ width: '100px' }} onError={handleErrorCam} onScan={handleScanCam}></QrReader>
+    <h3> Scanned by webcam code: {scanResult}</h3>
+  </div>
   )
 }
 
