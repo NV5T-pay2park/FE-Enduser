22178b41fb2cd3fe9226ea36015cc63de69873ac
htthtt12t1 <huathanh2503@gmail.com>
2022-07-13 22:14:33 +0700

    change feature to pages

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -5,6 +5,7 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
+import ListItem from '@mui/material/ListItem';
 
 ListCard.propTypes = {
     list: PropTypes.array,
@@ -14,19 +15,25 @@ function ListCard({ list }) {
 
     return (
         <div>
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
                 {list.map((value) => {
                     const labelId = `checkbox-list-secondary-label-${value.id}`;
                     return (
-                        <ListItemButton>
-                            <ListItemAvatar>
-                                <Avatar
-                                    src={`/src/img/Garage/img.png`}
-                                >
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText id={labelId} primary={value.name} secondary={value.address}></ListItemText>
-                        </ListItemButton>
+                        <ListItem key={value.id}
+                            disablePadding
+                        >
+
+
+                            <ListItemButton>
+                                <ListItemAvatar>
+                                    <Avatar
+                                        src={`/src/img/Garage/img.png`}
+                                    >
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText id={labelId} primary={value.name} secondary={value.address}></ListItemText>
+                            </ListItemButton>
+                        </ListItem>
                     )
                 })}
             </List>
