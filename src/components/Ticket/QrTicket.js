8b15e24a1d72c6c29f5e79808d88c9cf8a9fd846
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-07-15 08:44:22 +0700

    add mock fetch api

--- src/components/Ticket/QrTicket.js
+++ src/components/Ticket/QrTicket.js
@@ -3,7 +3,8 @@ import { QRCode } from 'react-qrcode-logo';
 
 const QrTicket = () => {
   return (
-    <QRCode value="https://zalopay.vn" size='140' logoImage='./ZaloPay-logo.png' logoWidth='40' logoHeight='40' bgColor='#FFFFFF'/>
+    // <QRCode value="https://zalopay.vn" size='140' logoImage='./ZaloPay-logo.png' logoWidth='40' logoHeight='40' bgColor='#FFFFFF'/>
+    <QRCode value='{"parkingid": 1244531324}' size='140' logoImage='./ZaloPay-logo.png' logoWidth='40' logoHeight='40' bgColor='#FFFFFF'/>
   )
 }
 
--- src/pages/qrcode/pages/index.js
+++ src/pages/qrcode/pages/index.js
@@ -1,24 +1,51 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { QrReader } from 'react-qr-reader';
 const QrPage = () => {
 
   const [scanResult, setScanResult] = useState('');
+  //const [parkingId, setParkingId] = useState('')
+  //const [ticketData, setTicketData] = useState({})
+  let ticketData
+  let parkingId
 
   const handleErrorCam = (error) => {
     console.log(error);
   }
 
+  // call checkin api
+  useEffect(() => {
+    console.log("call checkin api")
+    const headers = { 'Content-Type': 'application/json' }
+    console.log('id: ' + parkingId)
+    if (parkingId) {
+      fetch('https://jsonplaceholder.typicode.com/posts/1', { headers })
+          .then(response => response.json())
+          .then(data => ticketData = data);
+    
+      console.log(JSON.stringify(ticketData))
+    }
+  }, [parkingId])
+
+
   const handleScanCam = (result) => {
-    if (result) {
+    console.log("call handleScanCam")
+    console.log("result: " + result)
+    if (result !== undefined) {
       window.open(result.text);
-      setScanResult(result.text);
+      
+      //setScanResult(result.text);
+      console.log(JSON.stringify(result.text))
+      const obj = JSON.parse(result.text);
+      parkingId = obj.parkingId
+      //setParkingId(obj.parkingId)
+      // console.log("\n\n" + obj.parkingid)
     }
   }
 
   return (
     <div style={{ backgroundColor: 'red', height: 'calc(100vh - 112px)' }}>
-      <QrReader delay={1000} style={{ width: '100%', height: '100%', backgroundColor: 'red' }} onError={handleErrorCam} onResult={handleScanCam}></QrReader>
+      <QrReader delay={20000} style={{ width: '100%', height: '100%', backgroundColor: 'red' }} onError={handleErrorCam} onResult={handleScanCam}></QrReader>
 
     </div>
   )
