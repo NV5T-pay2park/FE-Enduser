195438b50d3d67644581459c03ab33e52307eae5
tanht <tanht@vng.com.vn>
2022-07-18 14:13:34 +0700

    [update] detail cảard

--- src/components/PaperCard/index.js
+++ src/components/PaperCard/index.js
@@ -7,13 +7,11 @@ import {DataGarage} from '../../models/Garage';
 
 
 PaperCard.propTypes = {
-  value: PropTypes.object,
 };
 
 function PaperCard() {
   var {value} = useParams();
   value = value.substr(1);
-  console.log(value);
   let v = DataGarage[0];
   for(let i = 0; i < DataGarage.length; i++)
     if (DataGarage[i].id === value) {
@@ -22,29 +20,35 @@ function PaperCard() {
     }
   
   return (
-    <Card sx={{ maxWidth: '80vw', height: '70vh', marginTop: '0px', borderRadius: '20px', minWidth: '80vw', align: "center" }} variant="outlined">
+    <div style={ {flexDirection: 'row', maxHeight: 'calc(100vh - 112px)' }}>
+    <Card sx={{ maxWidth: '80vw', height: '70vh', marginTop: '0px', borderRadius: '20px', minWidth: '80vw', align: "center",  }} variant="outlined">
       <CardContent>
         <Typography gutterBottom variant="h6" component="div" align="center">
           Thông tin chi tiết
         </Typography>
       </CardContent>
       <Box alignItems="center" justifyContent="center" display="flex">
-      
-          <Avatar sx={{ width: 80, height: 80 }}>
-                
+        <Avatar sx={{ width: 80, height: 80 }}>
               </Avatar>
-        
       </Box>
-      <CardContent>
+      <CardContent style={{ lineHeight: "24px" }}>
         <Typography gutterBottom variant="h8" component="div" align='center' >
           <b>{v.name}</b> 
         </Typography>
-        <Typography variant="h7" color="text.secondary" component="div">
-          {v.address}
+        <Typography variant="h7" component="div">
+          <b> Địa chỉ: </b>{v.address}
         </Typography>
-        <Typography variant="h7" color="text.secondary" component="div">
-          {v.phone}
-
+        <Typography variant="h7" component="div">
+          <b> Số điện thoại: </b>{v.phone}
+        </Typography>
+        <Typography variant="h7" component="div">
+          <b> Giờ làm việc: </b> {v.timeWorking}
+        </Typography>
+        <Typography variant='h7'>
+         <b> Trạng thái: </b>{v.status === "available" ? <div style={{color:'green'}}> Còn chỗ</div> : <div style={{color: 'red'}}> Hết chỗ</div> }
+        </Typography>
+        <Typography variant="h7" component="div" paragraph>
+          <b> Mô tả: </b> {v.discription}
         </Typography>
       </CardContent>
       {/* <CardActions>
@@ -52,6 +56,7 @@ function PaperCard() {
         <Button size="small">Learn More</Button>
       </CardActions> */}
     </Card>
+    </div>
   )
 }
 
