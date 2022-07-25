c0036c97d5ee9f39e90f3164b3c240fd752ce9f3
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-07-25 10:46:24 +0700

    trigger open payment page

--- src/features/Tickets/TicketCheckout.js
+++ src/features/Tickets/TicketCheckout.js
@@ -24,7 +24,7 @@ const TicketCheckout = () => {
       const post = res.data;
       console.log(post)
       stopPingRequest()
-      window.open("https://sbgateway.zalopay.vn/openinapp?order=eyJ6cHRyYW5zdG9rZW4iOiIyMjA3MjUwMDAwMDAwODYwOTJaNzBUIiwiYXBwaWQiOjk5OTg4OH0");
+      window.open("https://www.w3schools.com/jsref/met_win_open.asp");
     })
     .catch(error => console.log("err: " + error));
   }
