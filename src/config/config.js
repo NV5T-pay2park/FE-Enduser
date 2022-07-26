6de3e7286b1b54a900416e5259d515d47338f4c4
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-07-26 14:20:17 +0700

    add deployed server url

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const SERVER_BASE_URL = "http://172.25.147.70:8080"
+export const SERVER_BASE_URL = "https://pay2park.ep-eng.io"
 export const APP_NAME = "Pay2Park"
 export const END_USER_BASE_URL = "https://pay2park.vercel.app"
