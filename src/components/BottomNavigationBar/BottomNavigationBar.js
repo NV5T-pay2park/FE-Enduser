ba270824ef4030dc40df8ab38ab62aeb9c81271c
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-07-27 21:20:33 +0700

    add refactor for scan qr

--- src/components/BottomNavigationBar/BottomNavigationBar.js
+++ src/components/BottomNavigationBar/BottomNavigationBar.js
@@ -14,60 +14,89 @@ import css from './bottom.css'
 import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../../AppContext';
 import * as Constant from '../../config/config'
+import * as CheckInOutAPI from '../../api/checkInOut'
 
 const mockNewTicket = {"id":101,"checkInTime":"2022-07-18T07:27:48Z","checkOutTime":null,"licensePlates":"77C1-44094","vehicleType":{"id":1,"vehicleTypeName":"Xe máy","hibernateLazyInitializer":{}},"endUser":{"id":2,"firstName":"Partypooper009","lastName":"throwaway217217","gender":0,"phone":"0790529870","email":"throwaway217217@gmail.com"},"parkingLot":{"id":6,"parkingLotName":"Hiệp Phú","numberSlot":101,"numberSlotRemaining":101,"address":"Bình Chiểu, Thành phố Thủ Đức, TPHCM","status":0,"merchant":{"id":1,"name":"Thành phố Thủ Đức","represent":"Lee4an","email":"Lee4an@gmail.com","phone":"0906094163","hibernateLazyInitializer":{}},"lat":10.884166717529297,"ing":106.73027801513672,"timeOpen":5,"timeClose":22,"phoneNumber":"982347126","hibernateLazyInitializer":{}}}
 
 const BottomNavigationBar = () => {
   const [value, setValue] = React.useState(0);
-
   const navigate = useNavigate();
   const context = useContext(AppContext);
 
-  const scanWithZaloPayQR = () => {
+  const handleScanQR = async () => {
     if (window.ZaloPay.isZaloPay) {
-      const info = window.ZLP.Device().scanQRCode({ "needResult": 1, "scanType": 'qrCode'}).then(value => {        
+        await scanWithZaloPayQR()
+    } else {
+        navigate('/qr')
+    }
+  }
+
+  const scanWithZaloPayQR = async () => {
+      window.ZLP.Device().scanQRCode({ "needResult": 1, "scanType": 'qrCode'}).then(async (value) => {        
         let parkingId = value.page
         if (parkingId !== undefined) {
           window.ZaloPay.showLoading()
-          const url = Constant.SERVER_BASE_URL + '/api/checkIn'
-
-          fetch(url, {
-            method: 'POST', // or 'PUT'
-            headers: {
-              'Accept': 'application/json, text/plain',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              "endUserID": "3",
-              "parkingLotID": "14"
-            }),
-          })
-          .then((response) => response.json())
-          .then((ticketDataJSON) => {
-            const newTicket = ticketDataJSON.data
+          try {
+            const res = await CheckInOutAPI.requestCheckIn(2, 5)
+            const newTicket = res.data
             window.ZaloPay.hideLoading()
-            context.insertTicket(newTicket)
-            navigate('/')
-          })
-          .catch((error) => {
-            if (window.ZaloPay.isZaloPay) {
-              window.ZaloPay.showDialog({
-                title: "Checkin",
-                message: "Quét mã không thành công:" + JSON.stringify(error),
-                button: "OK"
-              });
+            if (newTicket.ticketID !== undefined) {
+                context.insertTicket(newTicket)
             }
-            window.ZaloPay.hideLoading()
-            // context.insertTicket(mockNewTicket)
             navigate('/')
-          });
-          }
-          return value 
-          })
-      } else {
-        navigate('/qr')
-      }
-    }
+          } catch (err) {
+            window.ZaloPay.hideLoading()
+            navigate('/')
+          } 
+        }
+      })
+  }
+
+  // const scanWithZaloPayQR = () => {
+  //   if (window.ZaloPay.isZaloPay) {
+  //     const info = window.ZLP.Device().scanQRCode({ "needResult": 1, "scanType": 'qrCode'}).then(value => {        
+  //       let parkingId = value.page
+  //       if (parkingId !== undefined) {
+  //         window.ZaloPay.showLoading()
+  //         const url = Constant.SERVER_BASE_URL + '/api/checkIn'
+
+  //         fetch(url, {
+  //           method: 'POST', // or 'PUT'
+  //           headers: {
+  //             'Accept': 'application/json, text/plain',
+  //             'Content-Type': 'application/json',
+  //           },
+  //           body: JSON.stringify({
+  //             "endUserID": "3",
+  //             "parkingLotID": "14"
+  //           }),
+  //         })
+  //         .then((response) => response.json())
+  //         .then((ticketDataJSON) => {
+  //           const newTicket = ticketDataJSON.data
+  //           window.ZaloPay.hideLoading()
+  //           context.insertTicket(newTicket)
+  //           navigate('/')
+  //         })
+  //         .catch((error) => {
+  //           if (window.ZaloPay.isZaloPay) {
+  //             window.ZaloPay.showDialog({
+  //               title: "Checkin",
+  //               message: "Quét mã không thành công:" + JSON.stringify(error),
+  //               button: "OK"
+  //             });
+  //           }
+  //           window.ZaloPay.hideLoading()
+  //           // context.insertTicket(mockNewTicket)
+  //           navigate('/')
+  //         });
+  //         }
+  //         return value 
+  //         })
+  //     } else {
+  //       navigate('/qr')
+  //     }
+  //   }
 
   return (
     <BottomNavigation
@@ -80,7 +109,7 @@ const BottomNavigationBar = () => {
     >
       <BottomNavigationAction label="Vé" icon={<BookOnlineIcon />} onClick={() => navigate('/')} />
       {/* <BottomNavigationAction icon={<QrCodeScannerIcon />} onClick={() => navigate('/qr')} className={css.qr} /> */}
-      <BottomNavigationAction icon={<QrCodeScannerIcon />} onClick={scanWithZaloPayQR} className={css.qr} />
+      <BottomNavigationAction icon={<QrCodeScannerIcon />} onClick={handleScanQR} className={css.qr} />
       <BottomNavigationAction label="Tìm kiếm" icon={<LocationSearchingOutlinedIcon />} onClick={() => navigate('/search')} />
     </BottomNavigation>
 
