cfd792b50f8cea03db756b21bda8ba26e17d1fee
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-08-01 02:10:41 +0700

    add show dialog status checkout

--- src/api/ticketAPI.js
+++ src/api/ticketAPI.js
@@ -1,5 +1,19 @@
 import * as Constant from '../config/config'
 
+const mockTicket = {
+    "ticketID": 15,
+    "checkInTime": "2022-07-18T14:27:48Z",
+    "checkOutTime": null,
+    "amount": 3000,
+    "licensePlate": "77C1-27549",
+    "vehicleType": "Xe máy",
+    "endUserID": 6,
+    "endUserName": "TheeeHman Smifter",
+    "parkingLotID": 4,
+    "parkingLotName": "Ngô Quyền",
+    "status": true
+}
+
 export const getTicketByEndUserId = async (id) => {
     const apiResponseData = await fetch(Constant.SERVER_BASE_URL + `/api/getTicketByEndUserId?endUserID=${id}`)
     if(!apiResponseData.ok){
@@ -18,6 +32,6 @@ export const getTicketByID = async (ticketID) => {
     }
     const ticketDataJSON = await apiResponseData.json()
     const ticketData = ticketDataJSON?.data
-    if (ticketData === null || ticketData === undefined || ticketData === "") return []
+    if (ticketData === null || ticketData === undefined || ticketData === "") return mockTicket
     return ticketData
 }
