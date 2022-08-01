2de9e7844d7ae9117735b32c8ae81ae23d443a88
tanht <tanht@vng.com.vn>
2022-08-01 20:53:39 +0700

    debug

--- src/components/Card/ParkingCard.js
+++ src/components/Card/ParkingCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { CardMedia, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import * as Service from '../../services/index';
 
 const CardMedia = React.lazy(() => import('@mui/material/CardMedia'));
